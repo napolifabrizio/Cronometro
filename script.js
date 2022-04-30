b579cb153c816b79ba8651a3a099548b2247f5ce
Fabrizio <fabrizionapoli50@gmail.com>
2022-04-30 20:45:04 -0300

    modificacao

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
-let croSeg = document.getElementById('segundos')
-let croMin = document.getElementById('minutos')
-let croHour = document.getElementById('horas')
-let start = document.getElementById('start')
-let stop = document.getElementById('stop')
-let numero = 0
-let min = 1
-let hour = 1
+//Cada 0 do cronometro, elementos antes do evento click
+let seg2 = document.getElementsByTagName('p')[7]
+let seg1 = document.getElementsByTagName('p')[6]
+let min1 = document.getElementsByTagName('p')[3]
+let min2 = document.getElementsByTagName('p')[4]
+let hour1 = document.getElementsByTagName('p')[0]
+let hour2 = document.getElementsByTagName('p')[1]
 
-let seg
-let minu
-let hh
+//button for start
+let button = document.getElementsByTagName('button')[0]
 
-start.addEventListener('click', init)
+//valor que cada elemento vai receber para mudar o tempo
 
-function init() {
-    seg = setInterval(segundos, 1000)
-    minu = setInterval(minutos, (60*1000)+1)
-    hh = setInterval(horas, (60*60*1000)+2)
+let segundo1 = 1
+let segundo2 = 0
+let minuto1 = 1
+let minuto2 = 1
+let hours1 = 1
+let hours2 = 1
 
+button.addEventListener('click', intervalo)
+
+function intervalo() {
+    setInterval(iniciar, 0.5)
 }
 
-function segundos() {
-    if (numero >= 60) {
-        numero = 1
-        
+function iniciar() {
+    if (segundo2 >= 10) {
+        segundo2 = 0
+        seg1.innerHTML = segundo1
+        segundo1++
     } else {
-        croSeg.innerHTML = numero
-        numero++
+        seg2.innerHTML = segundo2
+        segundo2++
+    }
+
+    if(segundo1>=6){
+        segundo1 = 0
+        min2.innerHTML = minuto2
+        minuto2++
+    }
+    if(minuto2>=10){
+        minuto2=0
+        min1.innerHTML = minuto1
+        minuto1++ 
+    }
+    if(minuto1>=6){
+        minuto1 = 0
+        hour2.innerHTML = hours2
+        hours2++
+    }
+    if(hours2>=10){
+        hours2 = 0
+        hour1.innerHTML = hours1
+        hours1++
     }
 }
-
-function minutos(){
-    if(min>=60){
-        min = 1
-    }else{
-        croMin.innerHTML = min
-        min++
-    }
-}
-
-function horas(){
-    croHour.innerHTML = hour
-    hour++
-}
-
-stop.addEventListener('click', stoper)
-
-function stoper(){
-
-    clearInterval(seg)
-    clearInterval(minu)
-    clearInterval(hh)
-}
-
 
 
 
