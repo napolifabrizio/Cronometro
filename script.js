9070702b17295695d764d5050e8566a883485dcf
Fabrizio <fabrizionapoli50@gmail.com>
2022-05-07 12:27:53 -0300

    novo_js_semCss

--- script.js
+++ script.js
@@ -1,69 +1,49 @@
-//Cada 0 do cronometro, elementos antes do evento click
-let seg2 = document.getElementsByTagName('p')[7]
-let seg1 = document.getElementsByTagName('p')[6]
-let min1 = document.getElementsByTagName('p')[3]
-let min2 = document.getElementsByTagName('p')[4]
-let hour1 = document.getElementsByTagName('p')[0]
-let hour2 = document.getElementsByTagName('p')[1]
+//números do cronômetro
+let segundo = document.getElementsByTagName('p')[4]
+let minuto = document.getElementsByTagName('p')[2]
+let hora = document.getElementsByTagName('p')[0]
 
-//button for start and stop
-let button = document.getElementsByTagName('button')[0]
-let parar = document.getElementsByTagName('button')[1]
+//botões do cronômetro
+let init = document.getElementById('começar')
+let stop = document.getElementById('parar')
+
+//valores do cronômetro 
+let segundos = 1
+let minutos = 1
+let horas = 1
 
 //let para o intervalo
 let time
 
-//valor que cada elemento vai receber para mudar o tempo
+//ação para iniciar o cronômetro
+init.addEventListener('click', cronos)
 
-let segundo1 = 1
-let segundo2 = 0
-let minuto1 = 1
-let minuto2 = 1
-let hours1 = 1
-let hours2 = 1
+function cronos() {
+   time = setInterval(cronometro, 1)
+}
 
-button.addEventListener('click', intervalo)
-parar.addEventListener('click', stop)
+function cronometro() {
 
-function stop(){
+    if (segundos == 60) {
+        minuto.innerHTML = minutos
+        segundos = 0
+        minutos++
+    } 
+    else if(minutos ==60){
+        hora.innerHTML = horas
+        minutos = 0
+        horas++
+    }else {
+        segundo.innerHTML = segundos
+        segundos++
+    }
+
+
+}
+
+//ação para parar o cronômetro
+stop.addEventListener('click', function(){
     clearInterval(time)
-}
-
-function intervalo() {
-  time = setInterval(iniciar, 0.5)
-}
-
-function iniciar() {
-    if (segundo2 >= 10) {
-        segundo2 = 0
-        seg1.innerHTML = segundo1
-        segundo1++
-    } else {
-        seg2.innerHTML = segundo2
-        segundo2++
-    }
-
-    if(segundo1>=6){
-        segundo1 = 0
-        min2.innerHTML = minuto2
-        minuto2++
-    }
-    if(minuto2>=10){
-        minuto2=0
-        min1.innerHTML = minuto1
-        minuto1++ 
-    }
-    if(minuto1>=6){
-        minuto1 = 0
-        hour2.innerHTML = hours2
-        hours2++
-    }
-    if(hours2>=10){
-        hours2 = 0
-        hour1.innerHTML = hours1
-        hours1++
-    }
-}
-
+})
 
 
